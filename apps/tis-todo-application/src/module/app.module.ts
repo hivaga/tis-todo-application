a0ea11736990323e10d37688a6566404def5c47f
aleksandar hadzhimitov <alex.hadzhimitov@kbc.be>
2020-11-23 01:16:12 +0200

    Adding Angular Material Components to Project

--- apps/tis-todo-application/src/module/app.module.ts
+++ apps/tis-todo-application/src/module/app.module.ts
@@ -2,15 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {RouterModule} from "@angular/router";
 
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+
 import {ApplicationRoutes} from "./app.routes";
 import {MainPageComponent} from "../view/pages/main-page/main-page.component";
 import {HomePageComponent} from "../view/pages/home-page/home-page.component";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 
 const tisComponents = [MainPageComponent, HomePageComponent]
+const angularMaterialModules = [MatInputModule, MatButtonModule, MatIconModule]
 
 @NgModule({
   declarations: [...tisComponents],
-  imports: [BrowserModule, RouterModule.forRoot(ApplicationRoutes)],
+  imports: [BrowserModule,
+    RouterModule.forRoot(ApplicationRoutes),
+    BrowserAnimationsModule,
+    ...angularMaterialModules],
   providers: [],
   bootstrap: [MainPageComponent],
 })
